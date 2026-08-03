aa91f2c067577fd90507eed8231cd045d64be1db
digitalcarrrrd <245046299+digitalcarrrrd@users.noreply.github.com>
2026-08-03 22:47:06 +0500

    feat: move workshop landing page to yt1 route

---
+++ app/workshops/yt1/page.tsx
@@ -0,0 +1,6 @@
+import { OfferLanding } from "@/components/offers/OfferLanding";
+import { youtubeEmpireBuildersOffer } from "@/lib/offers/offers";
+
+export default function YoutubeEmpireBuildersWorkshopPage() {
+  return <OfferLanding offer={youtubeEmpireBuildersOffer} />;
+}
