1feb795ed011548d9c84f38e225a6b160110f40d
digitalcarrrrd <contentblogpk@gmail.com>
2026-08-13 15:59:40 -0700

    Deploy YT Empire Builders payment modal

--- app/academy/ytempirebuilder/page.tsx
+++ app/academy/ytempirebuilder/page.tsx
@@ -20,7 +20,7 @@ export default function YtEmpireBuildersPage() {
       }}
     >
       <iframe
-        src="/yt-empire-builders-v2.html?v=20260814"
+        src="/yt-empire-builders-v2.html?v=20260814-payment"
         title="YT Empire Builders"
         style={{
           display: "block",
