572ef11e13e612cf1274f577bf4715f17954b582
digitalcarrrrd <contentblogpk@gmail.com>
2026-08-13 15:11:54 -0700

    Redeploy YT Empire Builders v2

--- app/academy/ytempirebuilder/page.tsx
+++ app/academy/ytempirebuilder/page.tsx
@@ -20,7 +20,7 @@ export default function YtEmpireBuildersPage() {
       }}
     >
       <iframe
-        src="/yt-empire-builders-v2.html"
+        src="/yt-empire-builders-v2.html?v=20260814"
         title="YT Empire Builders"
         style={{
           display: "block",
