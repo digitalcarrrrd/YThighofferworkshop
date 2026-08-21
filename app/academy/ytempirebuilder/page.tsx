73519512ae731d1cf5025d61a6f3b572af59063d
digitalcarrrrd <245046299+digitalcarrrrd@users.noreply.github.com>
2026-08-22 02:48:56 +0500

    Drop 3 installments to PKR 3,900/mo, elevate 30k to VIP Elite Founder, remove main-screen coupon box, add 50% workshop coupon support in popup

--- app/academy/ytempirebuilder/page.tsx
+++ app/academy/ytempirebuilder/page.tsx
@@ -20,7 +20,7 @@ export default function YtEmpireBuildersPage() {
       }}
     >
       <iframe
-        src="/yt-empire-builders-v2.html?v=20260821-valuestack"
+        src="/yt-empire-builders-v2.html?v=20260822-elite-3900"
         title="YT Empire Builders"
         style={{
           display: "block",
